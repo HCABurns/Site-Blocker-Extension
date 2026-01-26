4ab6006648cf7e9241744e7651c6a59fdcf06fca
HCABurns <harrycaburns@gmail.com>
2026-01-26 21:28:59 +0000

    Adding function for time.
    Adding a function to retrieve the number of Days/hours/minutes/seconds until a website becomes unblocked.

--- popup.js
+++ popup.js
@@ -1,3 +1,22 @@
+function getTime(remainingMs){
+    const totalSeconds = Math.max(0, Math.floor(remainingMs / 1000));
+    const days = Math.floor(totalSeconds / 86400);
+    const hours = Math.floor((totalSeconds % 86400) / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    const seconds = totalSeconds % 60;
+
+    if (days > 0){
+      return `${days} day${days !== 1 ? "s" : ""} and ${hours} hour${hours !== 1 ? "s" : ""}`
+    }
+    else if (minutes > 60){
+      return `${hours} hour${hours !== 1 ? "s" : ""} and ${minutes} minutes${minutes !== 1 ? "s" : ""}`
+    }
+    else if (minutes > 0){
+      return `${minutes} minute${minutes !== 1 ? "s" : ""} and ${seconds} second${seconds !== 1 ? "s" : ""}`
+    }else{
+      return `${seconds} second${seconds !== 1 ? "s" : ""}`
+    }
+}
 
 (async () => {
   const siteEl = document.getElementById("currentSite");
@@ -34,6 +53,22 @@
   statusEl.style.display = "block";
   statusEl.textContent = "Blocking…";
 
+  function startCountdown(expiresAt) {
+    function update() {
+      const remainingMs = expiresAt - Date.now();
+
+      if (remainingMs <= 0) {
+        statusEl.textContent = "Block expired...";
+        clearInterval(timer);
+        return;
+      }
+      statusEl.textContent = `Unblocks in ` + getTime(remainingMs);
+    }
+
+    update(); // run immediately
+    const timer = setInterval(update, 1000);
+  } 
+
   // Send chrome block site request for duration user selected.
   chrome.runtime.sendMessage(
     { type: "BLOCK_SITE", domain, durationMinutes },
@@ -43,7 +78,11 @@
         if (durationMinutes === null){
           statusEl.textContent = "Blocked forever…";
         }else{
-          statusEl.textContent = `Blocked for ${durationMinutes} minutes`;
+          statusEl.textContent = `Blocked for ` + getTime(durationMinutes*60*1000);
+          chrome.storage.local.get("blocks", ({ blocks = [] }) => {
+          const block = blocks.find(b => b.domain === domain);
+          startCountdown(block.expiresAt);
+          });
         }
         // Force reload to show the page is blocked.
         chrome.tabs.reload(tab.id);
@@ -66,7 +105,8 @@
           const remainingMs = block.expiresAt - Date.now();
           const remainingMinutes = Math.max(1,Math.ceil(remainingMs / (60 * 1000)));
           // Display remaining time until it can be used or blocked again.
-          statusEl.textContent = `Unblocks in ${remainingMinutes} more minute${remainingMinutes !== 1 ? "s" : ""}`;
+          //statusEl.textContent = `Unblocks in ${remainingMinutes} more minute${remainingMinutes !== 1 ? "s" : ""}`;
+          startCountdown(block.expiresAt)
         }
       });
       }
